8fe176558bd26fb46184d8453a1a495769c3ea28
Brutos212 <fabio212@hotmail.com>
2021-06-01 18:51:37 -0300

    Está com um Bub

--- js/main.js
+++ js/main.js
@@ -59,8 +59,35 @@ window.addEventListener('scroll', scrollTop)
 
 /*===== MIXITUP FILTER PORTFOLIO =====*/
 
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
+
 /* ===== LINK ACTIVE PORTFOLIO =====*/
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
+
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(L => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(L => l.addEventListener('click', activePortfolio))
 
 /* ===== SWIPER CAROUSEL =====*/
-
+const mySwiper = new Swiper('.swiper-container', {
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+   
+  });
 /* ===== GSAP ANIMATION =====*/
